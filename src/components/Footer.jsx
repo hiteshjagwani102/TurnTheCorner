8625389f9c2776896c0d4a53204bb6cccdbe00b9
Hitesh Jagwani <60232373+hiteshjagwani102@users.noreply.github.com>
2024-01-09 23:25:59 +0530

    Revert "Revert "changes""
    This reverts commit d2d3319f68b6a836eb6b1aa622cdb5becbe64345.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,16 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <p>
-                Have some doubts. <a>Fill this from.</a> we'll get in touch.
+                Have any doubts? Get in touch.
+                 
             </p>
-            <h4>© Copyright Trademark. All Rights Reserved.</h4>
+            <p>
+            Call us directly{' '}
+                       <a href="tel:905720073">9057206073</a>  
+            </p>
+            <p>Mail us{' '}
+                       <a href="mailto:turnthecorner.kj@gmail.com">turnthecorner.kj@gmail.com</a></p>
+            <h4>© Copyright 2024 Turn the Corner. All Rights Reserved.</h4>
         </div>
     );
 };
--- src/components/Instructors.jsx
+++ src/components/Instructors.jsx
@@ -1,45 +1,33 @@
 import React from "react";
 import styles from "../styles/Instructors.module.scss";
 import cyberSecurityInstructor from "../assets/instructor1.jpg";
-import aiToolsInsturctor from "../assets/instructor2.jpg";
-import financeInstructor from "../assets/instructor3.jpg";
+import aiToolsInsturctor from "../assets/AI Tools..jpeg";
+import financeInstructor from "../assets/FL.jpg";
 
 const Instructors = () => {
     return (
         <div id="instructors" className={styles.instructors}>
             <h3>Meet our Instructors.</h3>
             <div className={styles.list}>
-                <div className={styles.box}>
-                    <img src={cyberSecurityInstructor}></img>
-                    <h4>Dr. Ananya Sharma</h4>
-                    <p>
-                        Dr. Sharma is a seasoned educator in computer science,
-                        blending academic expertise with industry insights. With
-                        a Ph.D. in Artificial Intelligence, she imparts
-                        cutting-edge knowledge, fostering a passion for
-                        innovation.
-                    </p>
-                </div>
+                 
                 <div className={styles.box}>
                     <img src={aiToolsInsturctor}></img>
-                    <h4>Prof. Vikram Singh</h4>
+                    <h4>Mr. Rohit Jagwani</h4>
                     <p>
-                        Prof. Singh, an authority in economics, brings a wealth
-                        of experience to the classroom. His dynamic teaching
-                        style and practical approach make complex economic
-                        theories accessible, preparing students for real-world
-                        challenges.
+                    TTC AI TOOLS EXPERT
+                    </p>
+                    <p>
+                    SOFTWARE ENGINEER WELL VERSED IN AI TOOLS
                     </p>
                 </div>
                 <div className={styles.box}>
                     <img src={financeInstructor}></img>
-                    <h4>Ms. Priya Patel</h4>
+                    <h4> CA Rishab Jain</h4>
                     <p>
-                        Ms. Patel is a charismatic language and literature
-                        instructor, specializing in English literature. With an
-                        M.A. in Linguistics, she inspires a love for language,
-                        fostering critical thinking and effective communication
-                        skills in her students.
+                TTC FINANCIAL LITERACY EXPERT.
+                </p>
+                        <p>
+                4+ YEARS EXPERIENCE
                     </p>
                 </div>
             </div>

9d48929c630673ba0eb9b3cabee1f51ab1d2b640
khush201 <155832468+khush201@users.noreply.github.com>
2024-01-09 16:59:24 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import styles from "../styles/Navbar.module.scss";
+import logo from "../assets/LOGO.png";
 const Navbar = () => {
     const [hide, setHide] = useState(true);
     return (
         <div id="navbar" className={styles.navbar}>
-            <div className={styles.logo}>Turn The Corners</div>
+            <div className={styles.logo}><img src={logo} alt="Turn the Corner" /></div>
             <div className={styles.list}>
+                <a href="#hero">Home</a>
                 <a href="#about">About</a>
                 <a href="#courses">Courses</a>
                 <a href="#testimonials">Testimonials</a>
@@ -14,6 +16,7 @@ const Navbar = () => {
             <div
                 className={`${styles.mobileList} ${hide ? styles.hidden : ""}`}
             >
+                <a href="#home">Home</a>
                 <a href="#about">About</a>
                 <a href="#courses">Courses</a>
                 <a href="#testimonials">Testimonials</a>
